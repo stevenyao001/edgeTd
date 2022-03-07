c1b2b5c95efbf848880323d9564c5a2e4682c1e6
jinaqiqi <m15711388274@163.com>
2022-03-07 19:46:17 +0800

    add tdengine

--- model/tdengine/user_tde.go
+++ model/tdengine/user_tde.go
@@ -16,9 +16,12 @@ type UserTd struct {
 
 type UserTdGK struct {
 	*tdengine.TdEngine
-	Num          int  `json:"num"`
-	Status       int  `json:"status"`
-	InstantEp    int  `json:"instant_ep"`
+	Num       int `json:"num"`
+	Status    int `json:"status"`
+	InstantEp int `json:"instant_ep"`
+	MegnetStatus bool `json:"megnet_status"`
+	Ia           int  `json:"ia"`
+	Ep           int  `json:"ep"`
 }
 
 type Input struct {
@@ -85,7 +88,7 @@ func (u *UserTd) Find() (res []UserTd, err error) {
 	return
 }*/
 
-func (u *UserTd)Find() error {
+func (u *UserTd) Find() error {
 	sqls := fmt.Sprintf("select ep, ia, megnet_status from megnet_001 limit 10")
 	rows, err := u.Db.Query(sqls)
 	if err != nil {
@@ -104,7 +107,7 @@ func (u *UserTd)Find() error {
 		fmt.Println("megnet_001---:", tmp)
 	}
 
-	sqlsGK := fmt.Sprintf("select instant_ep, num, status from megnet_gk_001 limit 10")
+	sqlsGK := fmt.Sprintf("select instant_ep, num, status, ep, ia, megnet_status from megnet_gk_001 limit 10")
 	rowsGK, err := u.Db.Query(sqlsGK)
 	if err != nil {
 		logger.ErrorLog("UserTde-Find", "执行sql报错", "", err)
@@ -114,9 +117,9 @@ func (u *UserTd)Find() error {
 
 	for rowsGK.Next() {
 		tmp := UserTdGK{}
-		err = rowsGK.Scan(&tmp.InstantEp, &tmp.Num, &tmp.Status)
+		err = rowsGK.Scan(&tmp.InstantEp, &tmp.Num, &tmp.Status,&tmp.Ep, &tmp.Ia, &tmp.MegnetStatus)
 		if err != nil {
-			logger.ErrorLog("UserTde-Find", "赋值报错", "", err)
+			logger.ErrorLog("UserTde-Find", "执行sql报错", "", err)
 			return err
 		}
 		fmt.Println("megnet_gk_001---:", tmp)
@@ -125,7 +128,7 @@ func (u *UserTd)Find() error {
 	return nil
 }
 
-func (u *UserTd)Insert(data *Input) error {
+func (u *UserTd) Insert(data *Input) error {
 	ts := time.Now().UnixMilli()
 	sqls := fmt.Sprintf("insert into megnet_001 (ts, ep, ia, megnet_status) values (%d, %d, %d, %t)", ts, data.Properties.Ep, data.Properties.Ia, data.Properties.MegnetStatus)
 	_, err := u.Db.Exec(sqls)
@@ -135,7 +138,7 @@ func (u *UserTd)Insert(data *Input) error {
 	}
 
 	tsGK := time.Now().UnixMilli()
-	sqlsGK := fmt.Sprintf("insert into megnet_gk_001 (ts, megnet_id, instant_ep, num, status) values (%d, %d, %d, %d, %d)", tsGK, ts, data.Properties.InstantEp, data.Properties.Num, data.Properties.Status)
+	sqlsGK := fmt.Sprintf("insert into megnet_gk_001 (ts, megnet_id, instant_ep, num, status, ep, ia, megnet_status) values (%d, %d, %d, %d, %d, %d, %d, %t)", tsGK, ts, data.Properties.InstantEp, data.Properties.Num, data.Properties.Status, data.Properties.Ep, data.Properties.Ia, data.Properties.MegnetStatus)
 	_, err = u.Db.Exec(sqlsGK)
 	if err != nil {
 		logger.ErrorLog("UserTde-Insert", "执行sql报错", "", err)
